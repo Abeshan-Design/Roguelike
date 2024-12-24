cea9cd72858261a62ced2c82734d225a415a9b34
Abeshan-Design <chabeshanjaved@hotmail.com>
2024-12-24 02:43:12 -0500

    update main.py
    update main to use procgen

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tcod
 from engine import Engine
 from input_handlers import EventHandler
 from entity import Entity
-from game_map import GameMap
+from procgen import generate_dungeon
 
 def main() -> None:
     screen_width = 80
@@ -17,9 +17,9 @@ def main() -> None:
     player = Entity(int(screen_width / 2), int(screen_height / 2 ), '@', (255, 255, 255))
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', (255, 255, 0))
     entities = {npc, player}
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(map_width, map_height)
     engine = Engine(entities = entities, event_handler = event_handler, game_map = game_map, player = player)
-    
+
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
